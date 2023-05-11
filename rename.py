d2a6c0e395c64772506a0a9ddd47518b105b6410
khanhuyengiang <khanhuyengiang@gmail.com>
2023-05-12 00:44:23 +0700

    Initial commit

---
+++ rename.py
@@ -0,0 +1,17 @@
+# Un-comment and run this code in your notebook:
+# (Note: No need to import qutip, change path file if needed)
+# with open("rename.py") as f:
+#    exec(f.read())
+
+from qutip import *
+
+annihilation = destroy
+creation = create
+coherent_density_matrix = coherent_dm
+expectation = expect
+fock_density_matrix = fock_dm
+identity = qeye
+thermal_density_matrix = thermal_dm
+sigma_x = sigmax
+sigma_z = sigmaz
+sigma_y = sigmay
